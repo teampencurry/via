8498425a2dad91bf9f91e590944b9c4618239ada
RMS <122594547+teampencurry@users.noreply.github.com>
2023-06-29 17:13:35 +0700

    Add files via upload

--- music.js
+++ music.js
@@ -2,33 +2,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const audio = new Audio();
     const playStopBtn = document.getElementById("play-stop-btn");
     const volumeSlider = document.getElementById("volume-slider");
-    
+    const papperElem = document.querySelector(".papper");
+
     const musicFilePath = "music/indila.mp3";
-    
+
     audio.src = musicFilePath;
-    audio.volume = volumeSlider.value / 100; // Mengatur volume awal
-    
+    audio.volume = 0.3; // Mengatur volume awal menjadi 30%
+
     let isPlaying = false;
-    
-    playStopBtn.addEventListener('click', () => {
-        if (isPlaying) {
-            audio.pause();
-            audio.currentTime = 0;
-            playStopBtn.innerHTML = '<i class="fas fa-play"></i>';
-            isPlaying = false;
-        } else {
-            audio.play()
-            .then(() => {
-                playStopBtn.innerHTML = '<i class="fas fa-stop"></i>';
-                isPlaying = true;
-            })
-            .catch((error) => {
-                console.log("Tidak dapat memutar musik: ", error);
-            });
-        }
-    });
-    
+
+    playStopBtn.addEventListener('click', togglePlayStop);
+    papperElem.addEventListener('click', togglePlayStop);
+    papperElem.addEventListener('touchstart', togglePlayStop);
+
     volumeSlider.addEventListener('input', () => {
         audio.volume = volumeSlider.value / 100;
     });
+
+    function togglePlayStop() {
+        if (!isPlaying) {
+            audio.play()
+                .then(() => {
+                    playStopBtn.innerHTML = '<i class="fas fa-stop"></i>';
+                    isPlaying = true;
+                })
+                .catch((error) => {
+                    console.log("Tidak dapat memutar musik: ", error);
+                });
+        }
+    }
 });
